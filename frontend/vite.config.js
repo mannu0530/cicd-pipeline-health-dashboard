154bf42b8af1d59786b996602c263867511621aa
mannu0530 <maheshbiloniya@gmail.com>
2025-08-26 12:22:23 +0530

    update host allow

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['localhost', '127.0.0.1', '0.0.0.0', 'ec2-107-20-211-209.compute-1.amazonaws.com'],
     port: 5173,
     host: true
   }
